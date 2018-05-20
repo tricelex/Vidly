fd5efc65750a7dfcf0686b68d55d48ffca81c946
Chuckz <tricelex@gmail.com>
2018-05-20 11:56:02 +0100

    Completed Part 2 ASP.NET Fundamentals

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,35 +4,46 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-//        // GET: Movies
-//        public ViewResult Index()
-//        {
-//            return View();
-//        }
+        // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek!"},
+                new Movie { Id = 2, Name = "Wall-e"}
+            };
+        }
 
         // GET: Movies/Random
         public ViewResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-//            var customers = new List<Customer>
-//            {
-//                new Customer { Name = "Customer 1" },
-//                new Customer { Name = "Customer 2" }
-//            };
-//
-//            var viewModel = new RandomMovieViewModel
-//            {
-//                Movie = movie,
-//                Customers = customers
-//            };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
 
-            //return View(viewModel);
-            return View(movie);
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
     }
 }
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -7,6 +7,8 @@ namespace Vidly.Models
 {
     public class Customer
     {
+        public int Id { get; set; }
 
+        public string Name { get; set; }
     }
 }
