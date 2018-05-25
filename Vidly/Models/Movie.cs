a67fd15ac92b7bfeb71c97a89201a31c1a9999e7
Chuckz <tricelex@gmail.com>
2018-05-25 17:44:30 +0100

    Completed Part 4

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Security.AccessControl;
-using System.Web;
 
 namespace Vidly.Models
 {
@@ -14,11 +10,14 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
+      
         public Genre Genre { get; set; }
 
         [Display(Name = "Genre")]
         [Required]
         public byte GenreId { get; set; }
+
         public DateTime DateAdded { get; set; }
 
         [Display(Name = "Release Date")]
