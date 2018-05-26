eacfa9f7775e0789d10052eaf2733445f11228c7
Chuckz <tricelex@gmail.com>
2018-05-26 07:08:40 +0100

    Completed Part 5- Implementing Validation

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -34,8 +34,24 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                
+                
+                
+                return View("CustomerForm", viewModel);
+
+            }
+
+
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -32,15 +32,47 @@ namespace Vidly.Controllers
             return View(movies);
         }
 
+        public ViewResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+
+            return View("MovieForm", viewModel);
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel(movie)
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+
+            return View("MovieForm", viewModel);
+        }
+
+
         public ActionResult Details(int id)
         {
             var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
 
-             if (movie == null)
-            return HttpNotFound();
+            if (movie == null)
+                return HttpNotFound();
 
             return View(movie);
         }
+
 
         // GET: Movies/Random
         public ViewResult Random()
@@ -54,51 +86,32 @@ namespace Vidly.Controllers
 
             var viewModel = new RandomMovieViewModel
             {
-                Movie = movie,
                 Customers = customers
             };
 
             return View(viewModel);
         }
 
-        public ViewResult New()
-        {
-            var genres = _context.Genres.ToList();
-            var viewModel = new MovieFormViewModel
-            {
-                Genres = genres
-            };
-
-
-            return View("MovieForm", viewModel);
-        }
-
-        public ActionResult Edit(int id)
-        {
-            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
-
-            if (movie == null)
-                return HttpNotFound();
-
-            var viewModel = new MovieFormViewModel
-            {
-                Movie = movie,
-                Genres = _context.Genres.ToList()
-            };
-            
-
-
-            return View("MovieForm", viewModel);
-        }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel(movie)
+                {
+                    Genres = _context.Genres.ToList()
+                };
+
+
+                return View("MovieForm", viewModel);
+            }
+
             if (movie.Id == 0)
             {
                 movie.DateAdded = DateTime.Now;
                 _context.Movies.Add(movie);
-
             }
             else
             {
@@ -110,7 +123,7 @@ namespace Vidly.Controllers
             }
 
             _context.SaveChanges();
-            
+
 
             return RedirectToAction("Index", "Movies");
         }
